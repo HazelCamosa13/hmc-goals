14a09dc022ea8504aac6fbfb993bd1b2c6054ac1
HazelCamosa13 <camosahazel79@gmail.com>
2025-08-27 10:03:37 +0800

    Firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyANwWy_WbHJ4SyQ7JN9FHGLW6M2ViYY520",
+  authDomain: "hmc-goals.firebaseapp.com",
+  projectId: "hmc-goals",
+  storageBucket: "hmc-goals.firebasestorage.app",
+  messagingSenderId: "954170976876",
+  appId: "1:954170976876:web:1e5418306abec95038c649"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
